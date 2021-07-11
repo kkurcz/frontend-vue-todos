926c629b13c58b1d9940e0345708413f5b00db1f
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-07-11 14:07:14 +0200

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <div class="container">
+      <h1>ToDo List Manager</h1>
+      <h6>
+        This app is powerded by Vue 3 | Vuex 4 | Axios | Rails 6 | SQLite 3
+      </h6>
+      <Todos />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todos from "@/components/Todos.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Todos,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
